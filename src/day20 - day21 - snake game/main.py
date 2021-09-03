148c85778fa8b41c156f582e71c381d0bbd53eba
Gabrielle <gabrielleribeiro2010@gmail.com>
2021-09-02 22:18:57 -0300

    Adding main

---
+++ src/day20 - day21 - snake game/main.py
@@ -0,0 +1,33 @@
+# Create the snake body
+# Move the snake
+# Control de snake
+# Detect collision with food
+# Create a scoreboard
+# Detect collision with wall
+# Detect collision with tail
+
+from turtle import Screen, Turtle
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
