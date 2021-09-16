2f5aa4f176688bd85fbc57f60484269d17aa8977
Gabrielle <gabrielleribeiro2010@gmail.com>
2021-09-15 23:23:59 -0300

    Adding high score system

--- src/day20 - day21 - snake game/snake.py
+++ src/day20 - day21 - snake game/snake.py
@@ -15,9 +15,11 @@ class Snake:
         self.create_snake()
         self.head = self.segment[0]
 
+
     def create_snake(self):
         for position in STARTING_POSITIONS:
             self.add_segment(position)
+
 
     def add_segment(self, position):
         snake_body = Turtle("square")
@@ -27,8 +29,18 @@ class Snake:
         snake_body.goto(position)
         self.segment.append(snake_body)
 
+
+    def reset(self):
+        for seg in self.segment:
+            seg.goto(1000, 1000)
+        self.segment.clear()
+        self.create_snake()
+        self.head = self.segment[0]
+
+
     def extend(self):
         self.add_segment(self.segment[-1].position())
+
 
     def move(self):
         for body_piece in range(len(self.segment)-1, 0, -1):
@@ -37,17 +49,21 @@ class Snake:
             self.segment[body_piece].goto(new_x_position, new_y_position)
         self.head.forward(MOVE_DISTANCE)
 
+
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
+
 
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
+
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
+
 
     def right(self):
         if self.head.heading() != LEFT:
