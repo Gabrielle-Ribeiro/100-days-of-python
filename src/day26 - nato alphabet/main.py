bff27e5c5da0006764dcbe5cda775f13cb698f02
Gabrielle <gabrielleribeiro2010@gmail.com>
2021-09-30 21:05:53 -0300

    Adding exception handling i nato alphabet project

--- src/day26 - nato alphabet/main.py
+++ src/day26 - nato alphabet/main.py
@@ -4,7 +4,12 @@ alphabet_df = pd.read_csv('nato_phonetic_alphabet.csv')
 
 phonetic_dictinary = {row.letter:row.code for index, row in alphabet_df.iterrows()}
 
-user_input = input().upper()
-phonetic_code = [phonetic_dictinary[letter] for letter in user_input]
+phonetic_code = ""
+while len(phonetic_code) == 0:
+    user_input = input().upper()
+    try:
+        phonetic_code = [phonetic_dictinary[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
 
 print(phonetic_code)
